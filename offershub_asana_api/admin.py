faf0ea7e89e6076776c88f60318d2fd53df8f94f
conber1 <48555903+conber1@users.noreply.github.com>
2020-06-21 21:10:51 +0300

    Update admin.py

--- offershub_asana_api/admin.py
+++ offershub_asana_api/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from .models import *
 
-class UserAdmin(admin.ModelAdmin):
-    list_display, fields = ('user_name', 'user_email'), ('user_name', 'user_email') #List of fields which are displaying in django admin
-
-
 class ProjectAdmin(admin.ModelAdmin):
     list_display, fields = ('project_name',), ('project_name',)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display, fields = ('user_name', 'user_email'), ('user_name', 'user_email') #List of fields which are displaying in django admin
 
 class TaskAdmin(admin.ModelAdmin):
     list_display, exclude = ('task_name', 'project_link', 'executor_link'), ('task_id',) #The same, the exclude tuple removing the field from django admin
